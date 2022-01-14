5befc45f792318785cca63ae687aa89aead52f00
jck-bit <jckkinyanjui@gmail.com>
2022-01-14 20:31:12 +0300

    Button

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ function App() {
              <Button variant='outline-primary'>Add Expense</Button>
            </Stack>
 
-           <div style={{dispaly:"grid", gridTemplateColumns:"repeat(auto-fill,minmax(300px, 1fr))", gap:"1rem" ,alignItems:"flex-start"}}>           
-           </div>
-           <BudgetCard name="entertainment" 
+           {/* <div style={{dispaly:"grid", gridTemplateColumns:"repeat(auto-fill,minmax(300px, 1fr))", gap:"1rem" ,alignItems:"flex-start"}}>           
+           </div> */}
+           <BudgetCard name="Entertainment" 
            gray
-           amount={700} 
+           amount={500} 
            max={1000}
            ></BudgetCard>
       </Container>
